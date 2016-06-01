9451bb2726ecfe2aaa6bf265486cd2e11df4c5b6
paris-ci <jovart.arthur@gmail.com>
2016-06-01 20:12:43 +0200

    Base template

---
+++ main.py
@@ -0,0 +1,16 @@
+# -*- coding:Utf-8 -*-
+# !/usr/bin/env python3.5
+"""
+Discord-duckhunt -- main.py
+MODULE DESC 
+"""
+# Constants #
+
+__author__ = "Arthur — paris-ci"
+__licence__ = "WTFPL — 2016"
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
