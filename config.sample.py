bb0ceadd126a471dd7d826695d211e35a9657cb3
paris-ci <jovart.arthur@gmail.com>
2016-06-02 13:10:20 +0200

    Planification

---
+++ config.sample.py
@@ -0,0 +1,5 @@
+# -*- coding:Utf-8 -*-
+# !/usr/bin/env python3.5
+
+token = "Enter a bot token here"
+canardsJours = 99
--- main.py
+++ main.py
@@ -5,6 +5,9 @@ Discord-duckhunt -- main.py
 MODULE DESC 
 """
 # Constants #
+import random
+
+import time
 
 __author__ = "Arthur — paris-ci"
 __licence__ = "WTFPL — 2016"
@@ -22,6 +25,10 @@ import logging
 from logging.handlers import RotatingFileHandler
 
 import sys
+import discord
+import asyncio
+
+from config import *
 
 ## INIT ##
 logger = logging.getLogger("duckhunt")
@@ -41,9 +48,67 @@ steam_handler.setFormatter(formatter)
 logger.debug("Logger Initialisé")
 logger.info("Initialisation du programme, merci de patienter...")
 logger.debug("Version : " + str(sys.version))
+client = discord.Client()
 
-def main():
-    pass
+planification = {} #{"channel":[time objects]}
 
-if __name__ == '__main__':
-    main()
+def planifie():
+    global planification
+
+    planification_ = {}
+
+    for server in client.servers:
+        logger.debug("Serveur " + str(server))
+        for channel in server.channels:
+            if channel.type == discord.ChannelType.text:
+
+                logger.debug(" |- Check channel : " + channel.id)
+                permissions = channel.permissions_for(server.me)
+                if permissions.read_messages and permissions.send_messages:
+                    logger.debug("   |-Ajout channel : " + channel.id)
+                    templist = []
+                    now = time.time()
+                    thisDay = now - (now % 3600)
+                    for id in range(1, canardsJours + 1):
+                        templist.append(int(thisDay + random.randint(0,3600)))
+                    planification_[str(channel)] = templist
+
+    logger.debug("Nouvelle planification : " + str(planification_))
+
+    logger.debug("Supression de l'ancienne planification, et application de la nouvelle")
+    planification = planification_ #{"channel":[time objects]}
+
+
+
+@client.async_event
+def on_ready():
+    logger.info("Connecté comme " + str(client.user.name) + " | " + str(client.user.id))
+    logger.info("Creation de la planification")
+    planifie()
+    logger.info("Lancers de canards planifiés")
+    logger.info("Initialisation terminée :) Ce jeu, ca va faire un carton !")
+
+
+
+@client.async_event
+def on_message(message):
+    if message.author == client.user:
+        return
+
+
+
+    if message.content.startswith('!bang'):
+        counter = 0
+        tmp = yield from client.send_message(message.channel, 'BOUM')
+
+        time.sleep(1)
+        yield from client.edit_message(tmp, 'BOUM !'.format(counter))
+        time.sleep(2)
+        yield from client.edit_message(tmp, 'BANG !'.format(counter))
+
+
+    elif message.content.startswith('!sleep'):
+        yield from asyncio.sleep(5)
+        yield from client.send_message(message.channel, 'Done sleeping')
+
+client.run(token)
