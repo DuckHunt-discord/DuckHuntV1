3f468c5099c106c0f33ae7fbd56eeac6b0ddd54a
paris-ci <jovart.arthur@gmail.com>
2016-06-03 15:03:00 +0200

    Ajout de l'option de configuration pour supprimer les messages traités

--- config.sample.py
+++ config.sample.py
@@ -9,6 +9,8 @@ channelWL = False
 whitelist = [136797998217822208, 184672042770104321, 187961300704428033] # Ids des channels sur lequel le bot est actif
 tempsAttente = 5 * 60
 
+deleteCommands = True
+
 aideMsg = """Aide pour DuckHunt :
 ```Le but du jeu est de tuer les canards dès que vous les voyez ! C'est un peu un fps, mais pour discord :°)
 
