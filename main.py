0660717a82750e7cda834068b983c928934cb49e
paris-ci <jovart.arthur@gmail.com>
2016-07-03 17:21:02 +0200

    Afficher le nombre de SC tués

--- main.py
+++ main.py
@@ -468,10 +468,11 @@ def on_message(message):
                             database.addToStat(message.channel, message.author, "exp", int(getPref(message.server, "expParCanard") * (getPref(message.server, "SClevelmultiplier") * canardencours["level"])))
                             yield from asyncio.sleep(getPref(message.server, "lagOnBang"))
                             yield from client.edit_message(tmp, str(message.author.mention) + _(
-                                " > :skull_crossbones: **BOUM**\tTu l'as eu en {time} secondes, ce qui te fait un total de {total} canards sur #{channel}.     \_X<   *COUAC*   [{exp} xp]",
+                                " > :skull_crossbones: **BOUM**\tTu l'as eu en {time} secondes, ce qui te fait un total de {total} canards (dont {supercanards} supercanards) sur #{channel}.     \_X<   *COUAC*   [{exp} xp]",
                                 language).format(**{
                                 "time"   : int(now - canardencours["time"]), "total": database.getStat(message.channel, message.author, "canardsTues"),
-                                "channel": message.channel, "exp": int(getPref(message.server, "expParCanard") * (getPref(message.server, "SClevelmultiplier") * canardencours["level"]))
+                                "channel": message.channel, "exp": int(getPref(message.server, "expParCanard") * (getPref(message.server, "SClevelmultiplier") * canardencours["level"])),
+                                "supercanards" : database.getStat(message.channel, message.author, "superCanardsTues")
                             }))
                             if database.getStat(message.channel, message.author, "meilleurTemps",
                                                 default=getPref(message.server, "tempsAttente")) > int(
